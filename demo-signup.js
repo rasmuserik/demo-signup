b9e903716ba12ae030d1b21507834b3424da6452
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-05-10 14:07:41 +0200

    update-welcome-message

--- demo-signup.js
+++ demo-signup.js
@@ -23,8 +23,6 @@ let mailgun = require('mailgun-js')({
 let mailcomposer = require('mailcomposer');
 
 function mailDemoKey(addr) {
-  let demokey = ((Math.random() * 0x7fffffff / 2999) | 0) * 2999;
-
   let mail = mailcomposer({
     from: 'robot@solsort.com',
     to: addr,
@@ -32,54 +30,48 @@ function mailDemoKey(addr) {
     subject: 'BiblioteksApp.dk',
     text: `Hej,
 
-du får hermed adgang prototypen:
-
-http://demo.biblioteksapp.dk/#key${demokey}
-
-Adgangen er personlig og må ikke deles,
-- del i stedet link til biblioteksapp.dk
+tak for interessen,
+den nuværende demo kan ses på:
+http://demo.biblioteksapp.dk/
+og har ikke adgangsbegrænsning.
 
 Bemærk: det er en tidlig prototype,
 og ting vil ændre sig meget.
 
+Når næste udgave releases
+som lukket beta,
+får du også en mail
+med adgang hertil.
+
 Alt feedback er velkomment,
 og kan sendes til
 rasmuserik@solsort.com
-(som også har fået en kopi
-af denne mail, - send også
-en mail hertil hvis du vil
-framelde dig emails, da dette
-ikke er automatiseret endnu)
 
-Mange hilsner fra
-Post-robotten :)`,
-    html: `Hej,<br><br>
-du får hermed adgang prototypen:<br>
+Mange hilsner 
+Rasmus Erik :)`,
+    html: `Hej,<br>
 <br>
-
-<a href="http://demo.biblioteksapp.dk/#key${demokey}">
-http://demo.biblioteksapp.dk/#key${demokey}</a><br>
-<br>
-Adgangen er personlig og må ikke deles,<br>
-- del i stedet link til biblioteksapp.dk<br>
+tak for interessen,<br>
+den nuværende demo kan ses på:<br>
+<a href="http://demo.biblioteksapp.dk/">http://demo.biblioteksapp.dk/</a><br>
+og har ikke adgangsbegrænsning.<br>
 <br>
 Bemærk: det er en tidlig prototype,<br>
 og ting vil ændre sig meget.<br>
 <br>
+Når næste udgave releases<br>
+som lukket beta,<br>
+får du også en mail<br>
+med adgang hertil.<br>
+<br>
 Alt feedback er velkomment,<br>
 og kan sendes til<br>
-<a href="mailto:rasmuserik@solsort.com">rasmuserik@solsort.com</a><br>
-(som også har fået en kopi<br>
-af denne mail, - send også<br>
-en mail hertil hvis du vil<br>
-framelde dig emails, da dette<br>
-ikke er automatiseret endnu)<br>
+rasmuserik@solsort.com<br>
 <br>
-Mange hilsner fra<br>
-Post-robotten :)<br>
-<br>  
-<img width=2 height=2 src="https://incoming.solsort.com/log.js?bogappmail{demokey}">`
-  });
+Mange hilsner <br>
+Rasmus Erik :)<br>
+<br>
+<img width=2 height=2 src="https://incoming.solsort.com/log.js?bogappmail{demokey}">`});
 
   mail.build(function(err, msg) {
     if (err) {
